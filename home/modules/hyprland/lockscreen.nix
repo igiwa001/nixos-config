58c8ed828ee067cecb7e37df44460a24bd105e37
Igor Iwanicki <igor.iwanicki@protonmail.com>
2025-01-20 11:54:56 +0100

    hypridle: lock screen when laptop lid closes

--- home/modules/hyprland/lockscreen.nix
+++ home/modules/hyprland/lockscreen.nix
@@ -7,7 +7,13 @@
 }: {
   services.hypridle = {
     enable = true;
-    settings.general.lock_cmd = "pidof hyprlock || hyprlock";
+    settings = {
+      general = {
+        lock_cmd = "pidof hyprlock || hyprlock";
+        before_sleep_cmd = "loginctl lock-session";
+        after_sleep_cmd = "hyprctl dispatch dpms on";
+      };
+    };
   };
   programs.hyprlock = {
     enable = true;
