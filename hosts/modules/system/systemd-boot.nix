c99031dca9eb6f81ea7d36e7bd703ef44d70850f
Igor Iwanicki <igor.iwanicki@protonmail.com>
2025-04-10 21:08:27 +0200

    enable bsod screen

--- hosts/modules/system/systemd-boot.nix
+++ hosts/modules/system/systemd-boot.nix
@@ -5,12 +5,18 @@
   pkgs,
   ...
 }: {
-  boot.loader = {
-    efi.canTouchEfiVariables = true;
-    systemd-boot = {
-      enable = true;
-      configurationLimit = 3;
-      editor = false;
+  boot = {
+    # Initrd bsod screen
+    initrd.systemd.enable = true;
+
+    # Systemd bootloader
+    loader = {
+      efi.canTouchEfiVariables = true;
+      systemd-boot = {
+        enable = true;
+        configurationLimit = 3;
+        editor = false;
+      };
     };
   };
 }
