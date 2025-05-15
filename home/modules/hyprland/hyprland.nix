ed87b101062aa7a3b97125067571015d1e238029
Igor Iwanicki <igor.iwanicki@protonmail.com>
2025-05-15 09:53:14 +0200

    hyprland: second desktop monitor

--- home/modules/hyprland/hyprland.nix
+++ home/modules/hyprland/hyprland.nix
@@ -116,6 +116,7 @@
 
       monitor = [
         "desc:LG Electronics LG ULTRAGEAR 404NTGYA6311, preferred, 0x0, 1.33"
+        "desc:AOC 24P2W1DG5 PSMM1HA001150, highres, 1920x-400, 1.2, transform, 3"
         "desc:AOC 2470W GGJL1HA182550, highres, auto-up, auto"
       ];
     };
