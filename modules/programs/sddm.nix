cd6228bef171ef33c7b722befe1e6ef97d34e3e7
Igor Iwanicki <igor.iwanicki@protonmail.com>
2025-06-17 15:19:31 +0200

    modular: sddm disable wayland
    Both computers crashed with wayland enabled

--- modules/programs/sddm.nix
+++ modules/programs/sddm.nix
@@ -3,18 +3,26 @@
   config,
   ...
 }: {
-  options.settings.programs = {
-    sddm = lib.mkOption {
+  options.settings.programs.sddm = {
+    enable = lib.mkOption {
       type = lib.types.bool;
       default = true;
     };
+
+    wayland = lib.mkOption {
+      type = lib.types.bool;
+      default = false;
+    };
   };
 
-  config = lib.mkIf config.settings.programs.sddm {
-    services.displayManager.sddm = {
-      enable = true;
-      enableHidpi = true;
-      wayland.enable = true;
+  config = lib.mkIf config.settings.programs.sddm.enable {
+    services = {
+      displayManager.sddm = {
+        enable = true;
+        enableHidpi = true;
+        wayland.enable = config.settings.programs.sddm.wayland;
+      };
+      xserver.enable = lib.mkIf (!config.settings.programs.sddm.wayland) true;
     };
   };
 }
