a66ff5b741614cbf9e0dce27aed8e9851c595f1d
Igor Iwanicki <igor.iwanicki@protonmail.com>
2025-07-29 17:41:07 +0200

    GPU: Remove radeon9070 page-fault workaround

--- hosts/desktop-modular/configuration.nix
+++ hosts/desktop-modular/configuration.nix
@@ -19,16 +19,4 @@
   # TEMPORARY
   settings.neovim.enable = false;
   settings.nvim-old.enable = true;
-
-  # AMDGPU crash workaround
-  boot.kernelPackages = lib.mkForce (pkgs.linuxPackagesFor (pkgs.linuxKernel.kernels.linux_6_12.override {
-    argsOverride = rec {
-      src = pkgs.fetchurl {
-        url = "mirror://kernel/linux/kernel/v${lib.versions.major version}.x/linux-${version}.tar.xz";
-        sha256 = "b04c5b3e5df6e0aa5e9cd1efe527fac99f9dd39a43b97f13b22f8ca93e524ba7";
-      };
-      version = "6.12.31";
-      modDirVersion = "6.12.31";
-    };
-  }));
 }
