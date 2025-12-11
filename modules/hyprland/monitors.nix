ff4219c456599ad2a125c30dea5f2104aa9f4b28
Igor Iwanicki <igor.iwanicki@protonmail.com>
2025-12-11 08:48:56 +0100

    hyprland: add monitor configuration

--- modules/hyprland/monitors.nix
+++ modules/hyprland/monitors.nix
@@ -11,5 +11,6 @@ in
       "desc:AOC 24P2W1DG5 PSMM1HA001150, highres, 1920x-400, 1.2, transform, 3"
       "desc:AOC 2470W GGJL1HA182550, highres, auto-up, auto"
       "desc:AOC Q27V5C 18DQ1HA025220, preferred, auto-up, 1.25"
+      "desc:Samsung Electric Company C27R50x H4CRB03181L, preferred, auto-up, auto"
     ];
   }
