22cd8c7996186515e762ded8651edb6adf55d8a3
Igor Iwanicki <igor.iwanicki@protonmail.com>
2026-04-14 15:42:40 +0200

    rpi4: enable 1-wire interface

--- hosts/rpi4/configuration.nix
+++ hosts/rpi4/configuration.nix
@@ -1,4 +1,8 @@
-{inputs, ...}: let
+{
+  inputs,
+  pkgs,
+  ...
+}: let
   inherit (inputs.nixos-hardware.nixosModules) raspberry-pi-4;
 in {
   imports = [
@@ -9,6 +13,23 @@ in {
   # Enable GPIO pins
   hardware.raspberry-pi."4".gpio.enable = true;
   settings.user.groups = ["gpio"];
+
+  # Enable 1-wire interface
+  boot.kernelModules = ["pwm_bcm2835" "w1-gpio" "w1-therm"];
+  hardware.deviceTree = {
+    enable = true;
+    filter = "*-rpi-*.dtb";
+    overlays = [
+      {
+        name = "pwm-2chan";
+        dtboFile = "${pkgs.device-tree_rpi.overlays}/pwm-2chan.dtbo";
+      }
+      {
+        name = "w1-gpio";
+        dtboFile = "${pkgs.device-tree_rpi.overlays}/w1-gpio.dtbo";
+      }
+    ];
+  };
 
   settings = {
     system.stateVersion = "25.05";
