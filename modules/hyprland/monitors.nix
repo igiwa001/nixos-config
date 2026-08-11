bfa14be61d405ec7771d605bc09d47359ae6fac5
Igor Iwanicki <igor.iwanicki@protonmail.com>
2026-08-11 23:07:52 +0200

    hyprland: add TV configuration

--- modules/hyprland/monitors.nix
+++ modules/hyprland/monitors.nix
@@ -7,11 +7,12 @@
 in
   lib.mkIf cfg.enable {
     settings.hyprland.settings.monitor = [
-      "desc:LG Electronics LG ULTRAGEAR 404NTGYA6311, preferred, 0x0, 1.33"
+      "desc:LG Electronics LG ULTRAGEAR 404NTGYA6311, preferred, 0x0, 1.33, bitdepth, 10"
       "desc:AOC 24P2W1DG5 PSMM1HA001150, highres, 1920x-400, 1.2, transform, 3"
       "desc:AOC 2470W GGJL1HA182550, highres, auto-up, auto"
       "desc:AOC Q27V5C 18DQ1HA025220, preferred, auto-up, 1.25"
       "desc:Samsung Electric Company C27R50x H4CRB03181L, preferred, auto-up, auto"
       "desc:Samsung Electric Company SAMSUNG H1AK500000, preferred, auto-up, 2"
+      "desc:Hisense Electric Co. Ltd. HISENSE 0x00000001, highres, auto, auto, mirror, desc:LG Electronics LG ULTRAGEAR 404NTGYA6311, bitdepth, 10"
     ];
   }
